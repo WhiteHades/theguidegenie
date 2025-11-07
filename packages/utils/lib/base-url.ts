c29634cc0c98f4ed1b474c9b6782c887d09f0bd1
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2025-11-07 23:20:55 +0100

    feat: netlify deployment config and url handling

--- packages/utils/lib/base-url.ts
+++ packages/utils/lib/base-url.ts
@@ -2,6 +2,13 @@ export const getBaseUrl = () => {
   if (process.env.NUXT_PUBLIC_SITE_URL) {
     return process.env.NUXT_PUBLIC_SITE_URL;
   }
+  // netlify provides URL and DEPLOY_PRIME_URL
+  if (process.env.URL) {
+    return process.env.URL;
+  }
+  if (process.env.DEPLOY_PRIME_URL) {
+    return process.env.DEPLOY_PRIME_URL;
+  }
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
